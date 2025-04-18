a85f57588bb957e1254ae83e5eb85fb89baa4b83
Amirreza <arazarioun83@gmail.com>
2025-04-18 23:47:42 +0330

    remove unused API fetch logic from HomePage component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,5 @@
 const HomePage = async () => {
-  const res = await fetch('http://localhost:3000/api/questions')
-  const data = await res.json()
-  
-  
-  return (<>
-  Home Page
-  </>);
-}
- 
+  return <>Home Page</>;
+};
+
 export default HomePage;
