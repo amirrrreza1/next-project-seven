377175553b9945f4f19ef78b3b8f746e14f81cfa
Amirreza <arazarioun83@gmail.com>
2025-04-21 20:24:51 +0330

    fix: update API URLs to use the production endpoint for fetching questions

--- src/app/(quiz)/quiz/[id]/page.jsx
+++ src/app/(quiz)/quiz/[id]/page.jsx
@@ -4,13 +4,13 @@ const QuizSinglePage = async ({ params }) => {
   const { id } = await params; // id of the question
 
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/questions/${id}`
+    `https://next-project-seven-eta.vercel.app/api/questions/${id}`
   );
 
   const data = await res.json();
 
   const allRes = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/questions`
+    `https://next-project-seven-eta.vercel.app/api/questions`
   );
 
   const allData = await allRes.json();
